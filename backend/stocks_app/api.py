0a007904550aa82aa84b009c6b6d7bb2787e9493
John Barrett <johnjbarrett22@gmail.com>
2019-06-16 01:27:51 -0700

    Set up backend API through the rest framework in Django

---
+++ backend/stocks_app/api.py
@@ -0,0 +1,11 @@
+from stocks_app.models import User
+from rest_framework import viewsets, permissions
+from . serializers import UserSerializer
+
+#User Viewset
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = UserSerializer
