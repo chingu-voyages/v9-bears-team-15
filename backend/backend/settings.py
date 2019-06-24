20d51e6be5e4fa366e57c1d33699ad298738b5fc
clockwerkz <clockwerkz@gmail.com>
2019-06-23 22:57:21 -0700

    initial frontend set up

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'stocks_app'
+    'stocks_app',
+    'frontend'
 ]
 
 MIDDLEWARE = [
