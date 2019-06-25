948569d77fc6b59b985136f91d0337cb0db8ea3e
Carlos Fins <clockwerkz@gmail.com>
2019-06-24 20:57:46 -0700

    working on stocksearch component

--- backend/frontend/src/components/App.js
+++ backend/frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
 /* Components */
@@ -7,8 +7,10 @@ import StockSearch from './stocks/StockSearch';
 class App extends Component {
     render() {
         return(
-            <h1>Bears Stock Game!</h1>
-            <StockSearch />
+            <Fragment>
+                <h1>Bears Stock Game!</h1>
+                <StockSearch />
+            </Fragment>
         )
     }
 }
--- backend/frontend/src/components/stocks/StockSearch.js
+++ backend/frontend/src/components/stocks/StockSearch.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 export default class StockSearch extends Component {
     state = {
-        name: ''
+        name: '',
+        stock: ''
     }
 
-    onChange(e) {
-        console.log(e.target.name, e.target.value);
-        //this.setState({ [e.target.name]:e.target.value })
-    }
+    onChange = (e) => this.setState({ [e.target.name]:e.target.value });
 
+    searchStock = (e) => {
+        e.preventDefault();
+    }
+    
     render() {
+        const { name, stock } = this.state;
         return (
-            <form>
-                <label htmlFor="name">Enter Stock:</label>
-                <input text name="name" onChange={this.onChange} value={this.state.name} />
-            </form>
+            <Fragment>
+                <form onSubmit={searchStock}>
+                    <label htmlFor="name">Enter Stock:</label>
+                    <input type="text" name="name" onChange={this.onChange} value={name} />
+                </form>
+                <p>
+                    { stock ? (`Stock: ${stock}`):('No Stock Selected')}
+                </p>
+            </Fragment>
         )
     }
 }
