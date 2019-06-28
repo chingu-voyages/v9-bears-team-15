62ed380dfe7e1714a2fa3ffe7ce7a3fed8d3517d
clockwerkz <clockwerkz@gmail.com>
2019-06-27 20:46:01 -0700

    switches model to a stock based model

--- backend/stocks_app/models.py
+++ backend/stocks_app/models.py
@@ -1,14 +1,8 @@
 from django.db import models
 
 class Stock(models.Model):
-    firstName = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50)
-    userName = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    stockSymbol = models.CharField(max_length=10, blank=False)
-    #purchasePrice = models.DecimalField(10,2)
-    #currentPrice = models.DecimalField(10,2)
+    stockSymbol = models.CharField(max_length=10)
+    purchasePrice = models.DecimalField(max_digits=10,decimal_places=2)
+    currentPrice = models.DecimalField(max_digits=10,decimal_places=2)
     purchased_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
