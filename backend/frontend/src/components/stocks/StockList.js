782704a8ee9ad1ef203e08cde5b4fbab95f803d7
clockwerkz <clockwerkz@gmail.com>
2019-06-29 13:28:31 -0700

    adds basic table formatting

--- backend/frontend/src/components/stocks/StockList.js
+++ backend/frontend/src/components/stocks/StockList.js
@@ -5,7 +5,7 @@ import { fetchStockList } from '../../actions/stocks';
 
 
 export class StockList extends Component {
-    
+
     static propTypes = {
         stocks: PropType.array.isRequired,
         fetchStockList: PropType.func.isRequired
@@ -20,15 +20,26 @@ export class StockList extends Component {
             <div>
                 <h2>List of Stocks</h2>
                 <table>
-                    {this.props.stocks.map((stock, i) =>(
-                        <tr key={i}>
-                            <td>{stock.stockSymbol}</td>
-                            <td>{stock.currentPrice}</td>
-                            <td>{stock.purchasePrice}</td>
-                            <td>{stock.purchase_at}</td>
-                            <td>{stock.updated_at}</td>
+                    <thead>
+                        <tr>
+                            <th>Stock Symbol</th>
+                            <th>Current Price</th>
+                            <th>Purchase Price</th>
+                            <th>Purchased On</th>
+                            <th>Last Updated</th>
                         </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.stocks.map((stock, i) =>(
+                            <tr key={i}>
+                                <td>{stock.stockSymbol}</td>
+                                <td>{stock.currentPrice}</td>
+                                <td>{stock.purchasePrice}</td>
+                                <td>{stock.purchase_at}</td>
+                                <td>{stock.updated_at}</td>
+                            </tr>
                     ))}
+                    </tbody>
                 </table>
             </div>
         )
