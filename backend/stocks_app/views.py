a95b9d31651b0a8d7f0020883948d6e694a7fa74
Carlos Fins <clockwerkz@gmail.com>
2019-06-30 10:22:12 -0700

    sotcksearch function in progress

--- backend/stocks_app/views.py
+++ backend/stocks_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def fetchStock(request):
+    key = 'YD43NGRDHKUNLAFD'
+    url = 'https://www.alphavantage.co/query'
+    params = 'function=GLOBAL_QUOTE&symbol=MSFT&apikey='
+    return render(request, 'This will be stock data')
