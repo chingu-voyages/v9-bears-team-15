bfd213432786d25fc986201af3716a7a22f0e5cd
clockwerkz <clockwerkz@gmail.com>
2019-06-30 12:32:12 -0700

    custom url route is working!

--- backend/stocks_app/api.py
+++ backend/stocks_app/api.py
@@ -1,5 +1,7 @@
 from .models import Stock
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action, detail_route, list_route
+from rest_framework.response import Response
 from .serializers import StockSerializer
 
 #User Viewset
@@ -9,3 +11,11 @@ class StockViewSet(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = StockSerializer
+
+    @list_route(methods=['get'])
+    def fetch_stock(self, request):
+        key = 'YD43NGRDHKUNLAFD'
+        url = 'https://www.alphavantage.co/query'
+        params = 'function=GLOBAL_QUOTE&symbol=MSFT&apikey='
+        print('This will be stock data')
+        return Response({'status':'stock search endpoint'})
