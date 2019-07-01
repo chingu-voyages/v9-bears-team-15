5c9d5d0b9dd5be04379234ae98f756e8a3976d44
Carlos Fins <clockwerkz@gmail.com>
2019-06-30 18:04:59 -0700

    updates front end to use backend sotck fetch route

--- backend/frontend/src/actions/stocks.js
+++ backend/frontend/src/actions/stocks.js
@@ -2,14 +2,14 @@ import { FETCH_STOCK, FETCH_ERROR, FETCH_STOCKLIST } from '../actions/types';
 import axios from 'axios';
 
 export const fetchPrice = symbol => dispatch => {
-    axios.get(`https://api.iextrading.com/1.0/tops?symbols=${symbol}`)
+    axios.get(`api/stocks/fetch_stock/?symbol=${symbol}`)
     .then(response => {
-        if (response.data.length > 0){
+        if (response.data){
             dispatch({
                 type: FETCH_STOCK,
                 payload: {
-                    symbol: response.data[0].symbol,
-                    lastSalePrice: response.data[0].lastSalePrice
+                    symbol: response.data['01. symbol'],
+                    lastSalePrice: Number.parseFloat(response.data['05. price']).toFixed(2)
                 }
             });
         } else {
